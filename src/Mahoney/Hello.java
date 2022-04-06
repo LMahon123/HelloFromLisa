2c924dd3cbcec39acd191ea06ccd9a36f0e96ec4
LMahon123 <lmahon65@byui.edu>
2022-04-05 20:34:35 -0600

    Modification

--- src/Mahoney/Hello.java
+++ src/Mahoney/Hello.java
@@ -2,6 +2,10 @@ package Mahoney;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Lisa");
+        System.out.println("Hello, my name is Lisa.");
+
+        String name = "Class Assignment";
+
+        System.out.println(String.format("Hello, How are you today, %s.", name));
     }
 }
